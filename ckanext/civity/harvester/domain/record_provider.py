516bd88745a63b52b1e227eeb8ec8a3790649092
Bas Vanmeulebrouk <bas@civity.nl>
2023-09-19 18:02:42 +0200

    moved Civity harvester from gisweb to location from which it is re-usable

---
+++ ckanext/civity/harvester/domain/record_provider.py
@@ -0,0 +1,15 @@
+class IRecordProvider:
+
+    def __init__(self):
+        pass
+
+    def get_record_ids(self):
+        """Get a dictionary with records from the harvest source."""
+
+        # TODO Python 3: use type and return annotations
+
+        pass
+
+    def get_record_by_id(self, guid):
+        """Get a record with a specific ID from the harvest source."""
+        pass
