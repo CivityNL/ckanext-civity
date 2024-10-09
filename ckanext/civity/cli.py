ddb8c2e3e4c3ea64adf259c7c50d9471a707376e
erik@civity.nl <erik@civity.nl>
2024-10-09 13:47:23 +0200

    Added result to helper to better specify actions

--- ckanext/civity/cli.py
+++ ckanext/civity/cli.py
@@ -19,15 +19,26 @@ def init():
 
     pkg_ids = [r[0] for r in model.Session.query(model.Package.id)]
     site_user = toolkit.get_action('get_site_user')({'model': model, 'ignore_auth': True}, {})
+    n = len(pkg_ids)
 
-    for pkg_id in pkg_ids:
+    for idx, pkg_id in enumerate(pkg_ids, start=1):
+        counter = "{}/{}".format(idx,n)
         try:
             pkg_dict = toolkit.get_action(u'package_show')({'model': model, 'ignore_auth': True}, {'id': pkg_id})
-            create_group_memberships_from_theme(
+            member_delete, member_create = create_group_memberships_from_theme(
                 {'model': model, 'ignore_auth': True, 'user': site_user['name']},
                 pkg_dict
             )
-            click.secho("Updated package: %s" % pkg_id, fg="green", bold=True)
+            if member_delete or member_create:
+                msg = "{} Updated package [{}]:".format(counter, pkg_id)
+                if member_delete:
+                    msg += ' removed from {}'.format(member_delete)
+                if member_create:
+                    msg += ' added to {}'.format(member_delete)
+                click.secho(msg, fg="green", bold=True)
+            else:
+                click.secho("{} Checked package [{}] :: no actions required".format(counter, pkg_id))
+
         except Exception as e:
             error_shout(e)
-            error_shout(u"Package '{}' was not found".format(pkg_id))
+            error_shout("{} Package '{}' was not found".format(counter, pkg_id))
