9e8d13e8f9f5d2ba26ebdddbb98c51e8fec033bd
CKAN User <gil@civity.nl>
2025-07-04 09:22:29 +0000

    Update views.py

--- ckanext/civity/views.py
+++ ckanext/civity/views.py
@@ -9,14 +9,12 @@ def accessibility():
     return toolkit.render('accessibility/index.html')
 
 
-civity_blueprint.add_url_rule(
-    rule='/accessibility',
-    view_func=accessibility,
-    methods=['GET']
-)
-
-
 def get_blueprints():
     if get_accessibility_info_enabled():
-        return [civity_blueprint]
-    return []
+        civity_blueprint.add_url_rule(
+            rule='/accessibility',
+            view_func=accessibility,
+            methods=['GET']
+        )
+
+    return [civity_blueprint]
